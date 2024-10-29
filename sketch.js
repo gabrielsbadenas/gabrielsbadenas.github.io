aa7f93f46c9ad848bf9ffea04913933ff675592c
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2024-10-29 02:31:55 +0000

    buttons

--- sketch.js
+++ sketch.js
@@ -3,6 +3,27 @@ let canvasHeight = 608; // Alto inicial del canvas
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  let topLeft=createButton('FIGHT'),
+  topRight=createButton('PARTY'),
+  bottomLeft=createButton('ITEM'),
+  bottomRight=createButton('RUN');
+  topLeft.id('FIGHT');
+  topLeft.position('100px','100px');
+  //topLeft.position(0,300);
+  //topRight.position(267,300);
+  //bottomLeft.position(0,450);
+  //bottomRight.position(267,450);
+  topLeft.style('width','267px')
+         .style('height','150px')
+  topRight.style('width','267px')
+          .style('height','150px')
+  bottomLeft.style('width','267px')
+            .style('height','150px')
+  bottomRight.style('width','267px')
+             .style('height','150px')
+  bottomLeft.id('ITEM');
+  bottomRight.id('RUN');
+  topRight.id('PARTY')
   textAlign(CENTER, CENTER);
 }
 
